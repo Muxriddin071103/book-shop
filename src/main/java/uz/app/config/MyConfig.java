347221737d2e4c2b4376104402acfa7b7dc58e40
Muxriddin071103 <kingofdark0703@gmail.com>
2025-02-22 13:03:59 +0500

    Bug fixed

--- src/main/java/uz/app/config/MyConfig.java
+++ src/main/java/uz/app/config/MyConfig.java
@@ -1,7 +1,5 @@
 package uz.app.config;
 
-
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
