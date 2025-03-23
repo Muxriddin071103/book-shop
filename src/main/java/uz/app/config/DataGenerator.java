e2feec5b240ad1bdf1a1b565c7435027da1d4401
Muxriddin071103 <kingofdark0703@gmail.com>
2025-03-23 21:13:47 +0500

    Data generator

---
+++ src/main/java/uz/app/config/DataGenerator.java
@@ -0,0 +1,42 @@
+package uz.app.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import uz.app.entity.User;
+import uz.app.entity.enums.Role;
+import uz.app.repository.UserRepository;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataGenerator {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public ApplicationRunner initializeSuperAdmin() {
+        return args -> {
+            if (!userRepository.existsByPhoneNumber("111")) {
+                User superAdmin = User.builder()
+                        .firstName("Super")
+                        .lastName("Admin")
+                        .phoneNumber("111")
+                        .password(passwordEncoder.encode("super111"))
+                        .role(Role.SUPER_ADMIN)
+                        .createdAt(LocalDateTime.now())
+                        .enabled(true)
+                        .build();
+
+                userRepository.save(superAdmin);
+                System.out.println("Default Super Admin created!");
+            } else {
+                System.out.println("Super Admin already exists.");
+            }
+        };
+    }
+}
